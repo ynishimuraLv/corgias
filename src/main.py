77d89752590a435d264e16c9472dec57fd89502d
y246ra <ynishimura@edu.k.u-tokyo.ac.jp>
2026-03-20 11:16:38 +0900

    Separate argument parser from main to parser.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-import argparse
 import os
 import pathlib
 import shutil
@@ -16,6 +15,7 @@ from statsmodels.stats.multitest import multipletests
 
 import src.asr as asr
 import src.calstat as calstat
+import src.parser as parser
 import src.profiling as profiling
 
 try:
@@ -198,101 +198,7 @@ def run_stat(args, options):
 
 
 def main():
-
-    parent_parser = argparse.ArgumentParser(
-        add_help=False,
-        description = 'CORGIAS'
-    )
-
-    parser = argparse.ArgumentParser(parents=[parent_parser])
-    subparsers = parser.add_subparsers(title='Sub-commands', dest='subparser_name',
-                                       parser_class = argparse.ArgumentParser)
-
-    subparser_name2parser = {}
-
-    def new_subparser(subparsers, parser_name, parser_description):
-        subpar = subparsers.add_parser(parser_name, description = parser_description,
-                                       help = parser_description,
-                                       formatter_class = argparse.RawTextHelpFormatter,
-                                       parents=[parent_parser])
-        subparser_name2parser = subpar
-        return subpar
-
-    asr_description = '\tPrepare trees with ancestral presence/absence states of ortholog for ASA or SEV profiling. \n' \
-        '\tThe ortholog table should be a CSV file but each ortholog is assmued to be evolved independently. \n' \
-        '\tExample usage:\n' \
-        '\t\tcorgias asr -t tree.nwk -d orthologs.csv -i 0 --work_dir pastml_result -c 4 --prediction_method ML\n\n'\
-        '\tNote: Recostruction should be performed by a maximum-likelihood (DOWNPASS) and maximum-parsimony method (ACCTRAN)\n' \
-        '\t      for ASA and SEV, respectively.\n'
-    profiling_description = '\tPerform phylogenetic profiling using a ortholog table (naive, rle, cwa, cotr)\n' \
-                            '\ta species tree (rle, cwa, cotr) and/or, ancestral state reconstruction results (asa, sev)\n' \
-                            '\tExample usages: \n' \
-                            '\t\tcorgias profiling -m naive -og orthologs.csv -o naive_out.csv -c -4 --gpu -nb 4\n' \
-                            '\t\tcorgias profiling -m rle -og orthologs.csv -t tree.nwk -o rle_out.csv -c 4 \n' \
-                            '\t\tcorgias profiling -m cwa -og orthologs.csv -t tree.nwk -o cwa_out.csv -c 4 \n' \
-                            '\t\tcorgias profiling -m asa -a pastml_result_folder -t tree.nwk -o asa_out.csv -c 4 \n' \
-                            '\t\tcorgias profiling -m cotr -og orthologs.csv -t tree.nwk -o cotr_out.csv -c 4 \n' \
-                            '\t\tcorgias profiling -m sev --a pastml_result_folder -t tree.nwk -o sev_out.csv -c 4 \n\n' \
-                            '\tNote: with --test 5, Run test will start using five orthologs. \n'
-    stat_description = '\tConduct statistical tests for phylogenetic profiling results.\n' \
-                       '\tExample usage:\n' \
-                       '\t\tcorgias stat -i profiling_result.csv -m naive -o stat_out.csv -c 4 \n'
-
-    def positive_int(value):
-        ivalue = int(value)
-        if ivalue <= 0:
-            raise argparse.ArgumentTypeError(f"{value} is not a valid positive integer")
-        return ivalue
-
-    asr_parser = new_subparser(subparsers, 'asr', asr_description)
-    asr_parser.add_argument('-t', '--tree', required=True)
-    asr_parser.add_argument('-m', '--prediction_method', choices=['MPPA','MAP','JOINT','DOWNPASS','ACCTRAN',
-                                                                  'DELTRAN', 'ML', 'MP'],
-                            default='ML')
-    asr_parser.add_argument('-d', '--data', required=True)
-    asr_parser.add_argument('-i', '--id_index', default=0, type=int)
-    asr_parser.add_argument('--work_dir', required=True)
-    asr_parser.add_argument('-c', '--cores', default=1)
-    asr_parser.add_argument('--test', type=positive_int, default=0)
-    asr_parser.add_argument('--tmp')
-    asr_parser.add_argument('--keep', action='store_true', default=False)
-
-    profiling_parser = new_subparser(subparsers, 'profiling', profiling_description)
-    profiling_parser.add_argument('-m', '--method', choices=['naive', 'rle', 'cwa', 'asa', 'cotr', 'sev'], required=True)
-    profiling_parser.add_argument('-og', '--og_table')
-    profiling_parser.add_argument('-a', '--asr_folder')
-    profiling_parser.add_argument('-o', '--output', required=True)
-    profiling_parser.add_argument('-t', '--tree')
-    profiling_parser.add_argument('-c', '--cores', type=positive_int, default=1)
-    profiling_parser.add_argument('--ignore_branch', action='store_true', default=False)
-    if CUPY_AVAILABLE:
-        profiling_parser.add_argument('--gpu', action='store_true', default=False)
-        profiling_parser.add_argument('-nb', '--num_blocks', type=positive_int, default=0)
-    profiling_parser.add_argument('--test', type=positive_int, default=0)
-
-    def valid_float(value: str):
-        fvalue = float(value)
-        if not (0 < fvalue <= 1):
-            raise argparse.ArgumentTypeError(f"{value} is not a valid float. It should be larger than 0 and smaller than 1")
-        return fvalue
-
-    stat_parser = new_subparser(subparsers, 'stat', stat_description)
-    stat_parser.add_argument('-i', '--input', required=True)
-    stat_parser.add_argument('-m', '--method', required=True,
-                             choices=['naive', 'rle', 'cwa', 'asa', 'cotr', 'sev'])
-    stat_parser.add_argument('-o', '--output')
-    stat_parser.add_argument('-d', '--direction',
-                             choices=['both', 'correlation', 'anti-correlation'],
-                             default='both')
-    stat_parser.add_argument('-c', '--cores', type=positive_int, default=1)
-    stat_parser.add_argument('-t', '--threthold', type=valid_float, default=0.05)
-    stat_parser.add_argument('-s', '--statistical_test',
-                             choices=['bonferroni', 'sidak', 'holm-sidak', 'simes-hochberg',
-                                      'hommel', 'fdr_bh', 'fdr_by', 'fdr_tsbh', 'fdr_tsbky'],
-                             default='fdr_bh')
-    stat_parser.add_argument('--only_signif', action='store_true', default=False)
-
-    args, options = parser.parse_known_args()
+    args, options = parser.parse_arguments()
 
     if args.subparser_name == 'asr':
         run_asr(args, options)
