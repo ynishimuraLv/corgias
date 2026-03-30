71212b1586cb6ff30e483e896a59cd2e12ffda34
y246ra <ynishimura@edu.k.u-tokyo.ac.jp>
2026-03-30 17:19:35 +0900

    Implement LCA by bitwise operation

--- src/profiling/asa.py
+++ src/profiling/asa.py
@@ -11,8 +11,9 @@ def run_asa(args):
     if args.test:
         pairs = islice(pairs, args.test)
 
+
     with Pool(processes=args.cores) as process:
-        result = pl.DataFrame(process.starmap_async(asa, pairs).get(),
+        result = pl.DataFrame(process.starmap_async(asa_by_bit, pairs).get(),
                             schema = weighted_schema,
                             orient='row')
 
@@ -45,6 +46,19 @@ def asa(tree_og1: tuple[str, str],
 
     return og1, og2, result['1'], result['2'], result['3'], result['0']
 
+def asa_by_bit(tree_og1: tuple[str, str],
+        tree_og2: tuple[str, str],
+        ignore_branch: bool = False):
+    tree1 = et.Tree(tree_og1[0], format=1)
+    tree2 = et.Tree(tree_og2[0], format=1)
+    og1 = tree_og1[1]
+    og2 = tree_og2[1]
+    merged_tree = merge_tree(tree1, og1, tree2, og2)
+    if ignore_branch:
+        result = count_by_ancestral_state_by_bit(merged_tree)
+    else:
+        result = correct_by_ancestral_state(merged_tree)
+    return og1, og2, result[3], result[1], result[2], result[0]
 
 def merge_tree(tree1: et.Tree, og1: str,
                tree2: et.Tree, og2: str):
@@ -52,7 +66,7 @@ def merge_tree(tree1: et.Tree, og1: str,
     for node, node1, node2 in zip(tree.traverse(),
                                   tree1.traverse(),
                                   tree2.traverse()):
-        node.trait = mix_trait(getattr(node1, og1), getattr(node2, og2))
+        node.trait = mix_trait2bit(getattr(node1, og1), getattr(node2, og2))
     return tree
 
 
@@ -69,6 +83,18 @@ def mix_trait(og1: str, og2: str):
         return '3'
     else:
         return '4'
+
+def mix_trait2bit(og1: str, og2: str):
+    if og1 == '0' and og2 == '0':
+        return 0b0001
+    elif og1 == '1' and og2 == '1':
+        return 0b1000
+    elif og1 == '1' and og2 == '0':
+        return 0b0010
+    elif og1 == '0' and og2 == '1':
+        return 0b0100
+    else:
+        return 0b0000
 
 
 def correct_genomes(node: et.Tree) -> float:
@@ -102,6 +128,33 @@ def count_by_ancestral_state(tree: et.Tree):
         result[tree.state] += 1
 
     return result
+
+def count_by_ancestral_state_by_bit(tree: et.Tree):
+    result = [0, 0, 0, 0]
+
+    for node in tree.traverse("postorder"):
+
+        if node.is_leaf():
+            continue
+
+        for child in node.children:
+
+            diff = child.trait & ~node.trait
+
+            while diff:
+                i = (diff & -diff).bit_length() - 1
+                result[i] += 1
+                diff &= diff - 1
+
+    # root
+    m = tree.trait
+    while m:
+        i = (m & -m).bit_length() - 1
+        result[i] += 1
+        m &= m - 1
+
+    return result
+
 
 
 def correct_by_ancestral_state(tree: et.Tree):
